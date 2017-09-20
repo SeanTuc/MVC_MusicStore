ba891f01f1ece5db710642eddd9c413a670d5302
SeanTuc <sean_tucker366@hotmail.com>
2017-09-20 11:52:27 -0400

    Initial commmit

---
+++ MVC music store/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_music_store
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVC music store/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_music_store.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            ViewBag.Message = "please select a Genre";
+            return View();
+        }
+        // GET: Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            // Send Genre back to the view
+            ViewBag.Genre = genre;
+            return View();
+        }
+    }
+}
